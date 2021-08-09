9b5b0aaa16426fd1ec0d2ddeaaf01fc4c56a08b3
Didier <dbahsa@egmail.com>
2021-08-09 18:22:54 +0200

    change main class to dataclass

--- tournament.py
+++ tournament.py
@@ -1,11 +1,39 @@
+#!/user/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import datetime
 from dataclasses import dataclass, field
 
+
+# tournaments = [tournament1, tournament2, tournament[w]] ; 'w' is the num of tournament
+# tournament[w] = ['name', [round1, round2, round3, round4, round[z]]] ; 'z' is the num of rounds in a Tournament
+
+# list of all tournaments
+tournaments = []
+
+
+def add_rounds():
+    pass
+
+def add_players_db_indexes():
+    pass
+
+def add_end_date_time():
+    pass
+
+@dataclass
 class Tournament:
+    """ This class helps create instances for the current tournament """
     name: str
     location: str
-    start_date: str
+    start_date: str # automatic input
+    time_control: str # choices: bullet, blitz, or coup rapide
     description: str
-    timer: str
     number_of_rounds: int = field(default=4)
-    end_date = str
-    
+    rounds: list = field(init=False, repr=False, default_factory=add_rounds)
+    players_db_indexes: list = field(init=False, repr=False, default_factory=add_players_db_indexes)
+    end_date: str = field(init=False, repr=False, default_factory=add_end_date_time)
+
+
+
