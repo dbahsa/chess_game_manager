d9ccc015a58b4908c78ff7bf16cddd6267f1906f
Didier <dbahsa@egmail.com>
2021-08-14 21:32:57 +0200

    number_of_rounds changed to number_of_turns

--- tournament.py
+++ tournament.py
@@ -38,7 +38,7 @@ class Tournament:
     date: str
     time_control: str # choices: bullet, blitz, or coup rapide
     description: str
-    number_of_rounds: int = field(default=4)
+    number_of_turns: int = field(default=4)
     rounds: list = field(init=False, repr=False, default_factory=add_rounds)
     players_db_indexes: list = field(init=False, repr=False, default_factory=add_players_db_indexes)
     single_tournament_db: dict = field(init=False, repr=False)
@@ -48,7 +48,7 @@ class Tournament:
         self.single_tournament_db = {"Nom du tournoi": self.name,
                                     "Lieu": self.location,
                                     "Date": self.date,
-                                    "Nombre de tour": self.number_of_rounds,
+                                    "Nombre de tour": self.number_of_turns,
                                     "Tournées": self.rounds,
                                     "Joueurs": self.players_db_indexes,
                                     "Contrôle du temps": self.time_control,
